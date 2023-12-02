0535e52aeb0edb2e6680641098689b711b9b5f9d
I6356800 <v.rallis@student.maastrichtuniversity.nl>
2023-12-02 18:49:15 +0100

    Please dont conflict

--- PauseScreen.java
+++ PauseScreen.java
@@ -1,35 +1,125 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class PauseScreen extends JFrame {
     public PauseScreen() {
-        JButton resumeButton = new JButton("Resume");
-        JButton exitButton = new JButton("Exit");
+        ImageIcon playIcon = new ImageIcon("images/game_over_screen/refresh-regular-60.png");
+        ImageIcon exitIcon = new ImageIcon("images/game_over_screen/exit-solid-60.png");
 
-        resumeButton.addActionListener(new ActionListener() {
+        JButton playButton = new JButton(playIcon);
+        JButton exitButton = new JButton(exitIcon);
+
+        playButton.setBorder(BorderFactory.createEmptyBorder());
+        playButton.setContentAreaFilled(false);
+
+        exitButton.setBorder(BorderFactory.createEmptyBorder());
+        exitButton.setContentAreaFilled(false);
+
+        setContentPane(new BackgroundPanel()); // sets the background which contains the welcome to pentris text and the
+                                               // pentominoes art.
+
+        // Adding ActionListener to buttons
+        playButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Resume game logic
+                // Code to execute when play button is clicked
+                System.out.println("continue button clicked!");
             }
         });
 
         exitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Code to execute when play button is clicked
+                System.out.println("exiting button clicked");
                 System.exit(0);
             }
         });
 
-        this.setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-        this.add(resumeButton);
-        this.add(exitButton);
-        this.pack();
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    // resumes game if the escape is pressed
+                    System.out.println("Resuming Game");
+                    System.exit(0);
+                }
+            }
+        });
+        setFocusable(true); // Make sure the frame can gain focus
+        requestFocusInWindow(); // Request focus to ensure key events are captured
+
+        // Layout setup
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        //needs to be removed
+        JLabel gameOverLabel = new JLabel(""); //is not being used
+        gameOverLabel.setHorizontalAlignment(JLabel.CENTER); //not being used
+
+        //constraints for the label for the icons
+        gbc.gridx = 0; // Column 0
+        gbc.gridy = 0; // Row 0
+        gbc.gridwidth = GridBagConstraints.REMAINDER; // component spans all columns
+        gbc.anchor = GridBagConstraints.CENTER; // center alignment
+        gbc.weightx = 1;
+        gbc.weighty = 0.1; // Adjust the vertical weight as needed
+        gbc.gridwidth = 1; //reset to default
+
+        //add(gameOverLabel, gbc);
+        // Resetting constraints for buttons
+        //gbc.gridy = 1; // Move to the next row
+        //gbc.weighty = 1; // Adjust for button spacing
+        //play button
+        //gbc.gridx = 0; // First column
+
+        add(playButton, gbc);
+        gbc.gridx = 1; // Second column
+        add(exitButton, gbc);
+
+        // Window Properties
+        ImageIcon img = new ImageIcon("images/pentomino_logo.png");
+        setIconImage(img.getImage());
+        //gameOverLabel.setFont(new Font("Arial", Font.BOLD, 30));
+        setTitle("Game Paused");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        pack();
+        setLocationRelativeTo(null); // Center on screen
+        setSize(520, 636);
+        setVisible(true);
+    }
+
+    // Custom JPanel class for the pentominoes background + the text
+    class BackgroundPanel extends JPanel {
+        private Image backgroundImage;
+
+        // Constructor to load the image
+        public BackgroundPanel() {
+            backgroundImage = new ImageIcon("images/game_over_screen/game_over_background.png").getImage();
+        }
+
+        // Override paintComponent
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            // Draw the background image
+            g.drawImage(backgroundImage, 0, 0, this.getWidth(), this.getHeight(), this);
+        }
     }
 
     public static void main(String[] args) {
-        new PauseScreen();
+        // Ensuring GUI is created on Event Dispatch Thread because apparently it is
+        // better
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new PauseScreen();
+            }
+        });
     }
 }
